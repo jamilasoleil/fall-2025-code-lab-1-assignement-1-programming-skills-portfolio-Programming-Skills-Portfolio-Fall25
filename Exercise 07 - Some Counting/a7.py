5849c0c624d4c41073b239f15bd6035ddfafd85d
jamilasoleil <ja4m4licmas@gmail.com>
2025-11-23 22:32:17 +0400

    Add files via upload

---
+++ Exercise 07 - Some Counting/a7.py
@@ -0,0 +1,27 @@
+for i in range(1, 50, 1):
+    print(i, end=' ')
+#this line of code writes a loop that counts up from 0 to 50 in increments of 1
+
+print("\n") #space
+
+for i in range(50, 51, -1):
+    print (i, end=' ')
+#this line of code writes a loop that counts down from 50 to 0 in decrements of 1
+
+print("\n") #space
+
+for i in range(30, 51, 1):
+    print (i, end=' ')
+#this line of code writes a loop that counts up from 30 to 50 in increments of 1
+
+print("\n") #space
+
+for i in range(50, 10, -2):
+    print (i, end=' ')
+#this line of code writes a loop that counts down from 50 to 10 in decrements of 2
+
+print("\n") #space
+
+for i in range(100, 205, 5):
+    print (i, end=' ')
+#this line of code writes a loop that counts up from 100 to 200 in increments of 5
