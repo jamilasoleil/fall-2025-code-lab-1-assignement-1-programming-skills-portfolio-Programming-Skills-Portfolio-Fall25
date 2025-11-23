6751e0274b97bba6aa0c04be0c22ad4ba01a2ffc
jamilasoleil <ja4m4licmas@gmail.com>
2025-11-23 22:30:36 +0400

    Add files via upload

---
+++ Exercise 02- Simple Sums/a2.py
@@ -0,0 +1,11 @@
+A = 8
+#this line stores '8' in a variable
+
+B = 10
+#this line stores '10' in a variable
+
+C = A + B
+#this line adds A and B, then stores the result in C
+
+print(C)
+#this line prints C
