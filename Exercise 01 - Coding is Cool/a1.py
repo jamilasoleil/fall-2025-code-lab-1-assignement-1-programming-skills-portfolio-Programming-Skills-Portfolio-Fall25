4bf88d281c595efa17a04a90b0fc33a7900fb65b
jamilasoleil <ja4m4licmas@gmail.com>
2025-11-23 22:30:12 +0400

    Add files via upload

---
+++ Exercise 01 - Coding is Cool/a1.py
@@ -0,0 +1,10 @@
+A = "coding" 
+#this line stores 'coding' in a variable
+B = "is"
+#this line stores 'is' in a variable
+C = "cool"
+#this line stores 'cool' in a variable
+
+print (A, B, C, sep=" ")
+#this line prints all three variables together
+#the sep=" " adds a space between each word, making the output "coding is cool"
