8ec83aa172f5321bbc89cc6faedbee86e960f87e
jamilasoleil <ja4m4licmas@gmail.com>
2025-11-23 22:31:19 +0400

    Add files via upload

---
+++ Exercise 04 -Primitive Quiz/a4.py
@@ -0,0 +1,84 @@
+capital = input("What is the Captial of France? : ").title()
+Points = 0 
+#this line of code declares a variable for the points points 
+
+if capital == "Paris":
+    print ("Correct")
+    Points += 1 #this line of code adds a point when the user gets the answer right
+else:
+    print("Wrong the Answer is Paris")
+
+capital = input("What is the Captial of Portugal? : ").title()
+
+if capital == "Lisbon":
+    print ("Correct")
+    Points += 1 #this line of code adds a point when the user gets the answer right
+else:
+    print("Wrong the Answer is Lisbon")
+
+capital = input("What is the Captial of Hungray? : ").title()
+
+if capital == "Budapest":
+    print ("Correct")
+    Points += 1 #this line of code adds a point when the user gets the answer right
+else:
+    print("Wrong the Answer is Budapest")
+
+capital = input("What is the Captial of Norway? : ").title()
+
+if capital == "Oslo":
+    print ("Correct")
+    Points += 1 #this line of code adds a point when the user gets the answer right
+else:
+    print("Wrong the Answer is Olso")
+
+capital = input("What is the Captial of Greece? : ").title()
+
+if capital == "Athens":
+    print ("Correct")
+    Points += 1 #this line of code adds a point when the user gets the answer right
+else:
+    print("Wrong the Answer is Athens")
+
+capital = input("What is the Captial of Ireland? : ").title()
+
+if capital == "Dublin":
+    print ("Correct")
+    Points += 1 #this line of code adds a point when the user gets the answer right
+else:
+    print("Wrong the Answer is Dublin")
+
+capital = input("What is the Captial of Croatia? : ").title()
+
+if capital == "Zagreb":
+    print ("Correct")
+    Points += 1 #this line of code adds a point when the user gets the answer right
+else:
+    print("Wrong the Answer is Zagreb")
+
+capital = input("What is the Captial of Switzerland? : ").title()
+
+if capital == "Bern":
+    print ("Correct")
+    Points += 1 #this line of code adds a point when the user gets the answer right
+else:
+    print("Wrong the Answer is Bern")
+
+capital = input("What is the Captial of Slovakia? : ").title()
+
+if capital == "Bratislava":
+    print ("Correct")
+    Points += 1 #this line of code adds a point when the user gets the answer right
+else:
+    print("Wrong the Answer is Slovakia")
+
+capital = input("What is the Captial of Austria? : ").title()
+
+if capital == "Vienna":
+    print ("Correct")
+    Points += 1 #this line of code adds a point when the user gets the answer right
+else:
+    print("Wrong the Answer is Vienna")
+
+print(f"You Scored!: {Points}/10") 
+#this line of code outputs the final message
